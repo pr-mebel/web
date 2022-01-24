383fbfad5509e555fd960a4f6a5624ffe10f5f2d
Nikita Yakunin <nayakunin@outlook.com>
2022-01-24 22:32:54 +0300

    Test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -72,7 +72,7 @@ const useStyles = makeStyles((theme) => ({
 const HomePage: FC = () => {
     const classes = useStyles();
 
-    console.log(process.env);
+    console.log(JSON.stringify(process.env));
 
     return (
         <>

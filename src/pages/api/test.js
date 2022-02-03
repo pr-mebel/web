eaec315c57271542cfeaf3e0bc289c48bfe55291
Nikita Yakunin <nayakunin@outlook.com>
2022-02-03 07:57:14 +0300

    Remove tests

--- src/pages/api/test.js
+++
@@ -1,9 +0,0 @@
-import { withSentry } from '@sentry/nextjs';
-
-const handler = async (req, res) => {
-    throw new Error('API throw error test');
-    // eslint-disable-next-line no-unreachable
-    res.status(200).json({ name: 'John Doe' });
-};
-
-export default withSentry(handler);

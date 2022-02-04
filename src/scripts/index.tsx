3278bf379861d2df4e78e60fb6d0b0cc774b7c1e
Nikita Yakunin <nayakunin@avito.ru>
2022-02-04 16:58:26 +0300

    Fix scripts

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -20,26 +20,36 @@ export const ScriptsList = () => {
         <>
             <Script src="https://af.click.ru/af.js?id=10173" strategy="beforeInteractive" />
             <Script src="https://www.google-analytics.com/analytics.js" strategy="beforeInteractive" />
-            <Script src="//cdn.callibri.ru/callibri.js" type="text/javascript" strategy="afterInteractive" />
             <Script
                 id="google-tag-manager"
-                strategy="afterInteractive"
                 dangerouslySetInnerHTML={{
                     __html: GoogleTagManger,
                 }}
             />
             <Script
                 id="google-analytics"
-                strategy="beforeInteractive"
                 dangerouslySetInnerHTML={{
                     __html: GoogleAnalytics,
                 }}
             />
             <Script
                 id="yandex-metrika"
-                strategy="beforeInteractive"
                 dangerouslySetInnerHTML={{
                     __html: YandexMetrika,
+                }}
+            />
+            <Script
+                id="facebook-pixel"
+                dangerouslySetInnerHTML={{
+                    __html: FacebookPixel,
+                }}
+            />
+            <Script src="//cdn.callibri.ru/callibri.js" type="text/javascript" strategy="afterInteractive" />
+            <Script
+                id="mail.ru-raiting"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: MailRuCounter,
                 }}
             />
             <Script
@@ -47,20 +57,6 @@ export const ScriptsList = () => {
                 strategy="lazyOnload"
                 dangerouslySetInnerHTML={{
                     __html: BitrixMessenger,
-                }}
-            />
-            <Script
-                id="facebook-pixel"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: FacebookPixel,
-                }}
-            />
-            <Script
-                id="mail.ru-raiting"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: MailRuCounter,
                 }}
             />
         </>
